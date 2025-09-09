8ec48d60dcf36b6596d2307ff2054d628ec04ff3
xMathyu <xmathyu@gmail.com>
2025-09-09 12:14:39 -0500

    fix: Update test to reflect correct API response message

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "AI Transcripts Analyzer API is running!"', () => {
+      expect(appController.getHello()).toBe(
+        'AI Transcripts Analyzer API is running!',
+      );
     });
   });
 });
